4606a2147b356eb3c8dbbe6f8eddfaaedb79c0ba
Umer Irshad <68225540+umerirshad@users.noreply.github.com>
2022-01-17 21:37:39 +0500

    Update testimonials.js

--- js/testimonial.js
+++ js/testimonial.js
@@ -4,28 +4,28 @@ const testimonials =[
         name:"susan smith",
         job:"web developer",
         img:"https://images.unsplash.com/photo-1494708001911-679f5d15a946?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        text:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', ",
+        text:"Life isn't about finding yourself. Life is about creating yourself.",
     },
     {
         id: 2,
         name:"anna johnson",
         job:"web designer",
         img:"https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        text:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan.",
+        text:"I love deadlines. I love the whooshing noise they make as they go by.",
     },
     {
         id: 3,
         name:"peter jones",
         job:"intern",
         img:"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        text:"Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+        text:"It is better to be hated for what you are than to be loved for what you are not.",
      },
     {
         id: 4,
         name:"bill anderson",
         job:"the boss ",
         img:"https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=461&q=80",
-        text:"Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+        text:"Nothing ever really goes away--it just changes into something else.",
     },
 ];
 
